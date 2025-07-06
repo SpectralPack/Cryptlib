fb46f7435dae8b9b9dc12618cd909faa7880720c
Ruby <darkdragongaming379@gmail.com>
2025-07-06 14:07:05 +0100

    fix weird cyanness issue

--- main.lua
+++ main.lua
@@ -17,3 +17,14 @@ end
 G.C.SET["Tag"] = G.C.SET["Spectral"]
 G.C.SET["Blind"] = G.C.SET["Spectral"]
 G.C.SET["Content Set"] = HEX("6db67f")
+
+local ref = G.UIDEF.card_h_popup
+function G.UIDEF.card_h_popup(card)
+    if card.ability_UIBox_table then
+      local AUT = card.ability_UIBox_table
+        if not G.C.SET[AUT.card_type] then 
+            G.C.SET[AUT.card_type] = G.C.SET["Spectral"]
+        end
+    end
+    return ref(card)
+end
